8f3c9794e1163aaeae361d482701f88bb981e144
Andrew Siemon <sandr15@vt.edu>
2021-02-01 16:24:41 -0800

    Growth Hormone Calculator: Refactor and Clean Up
    * Refactored code to easily add more cases
    * Added function to accept user inputs
    * Cleaned up comments / structure

--- Growth Hormone Calculator.py
+++ Growth Hormone Calculator.py
@@ -1,8 +1,33 @@
-#Ty Stoner
-#This program takes a selected Growth Hormone Drug and converts to day supply
+#------------------------------------------------------------------------------#
+# Program: Growth Hormone Calculator
+# By: Ty Stoner
+#
+# Desc:
+# This program takes a selected Growth Hormone Drug and converts to day supply
+#------------------------------------------------------------------------------#
 
+#------------------------------------------------------------------------------#
+# Function: get_inputs
+# @brief
+# Prompt user for calculation inputs, return a tuple of the inputs
+#------------------------------------------------------------------------------#
+def get_inputs():
+    print("  Information for calculation")
+    print("-------------------------------")
 
+    Q = float(input("Number of Pens: "))
+    D = float(input("Dose (mg): "))
+    I = float(input("Injections per week: "))
 
+    if D < 0.025 or D > 2:
+        print("ERROR: D value", D, "is invalid!")
+        exit(1)
+
+    return (Q, D, I)
+
+#------------------------------------------------------------------------------#
+# Begin Main
+#------------------------------------------------------------------------------#
 print("Growth Hormone Calculator ")
 print("""
 This program is capable of calculating day supply for growth hormone drugs:
@@ -10,68 +35,34 @@ This program is capable of calculating day supply for growth hormone drugs:
     -------   ----------------
     A -- Norditropin Flexpro 5mg/1.5ml
     B -- Norditropin Flexpro 10mg/1.5ml
-    C -- 
 """)
 
-command=str(input("Select Drug: "))
+hormone_dict = {
+    "A" : 4.975,
+    "B" : 9.95       
+}
 
-if command == "A":
-    print ("Please provide following information for calculation")
-    Q = int(input("Number of Pens: "))
-    D = float(input("Dose (mg): "))
-    D1 = int(D)
-    I = int(input("Injections per week: "))
-    while True:
-        try:
-            input=int(D)
-        except ValueError:
-            print("That's not a number")
-        else:
-            if 0.025 <= input <= 2:
-                DS = (4.975*Q*(1/D)*(7/I))
-                print ('Days Supply: ',DS)
-                print ("""
-                Enter 2 for each 1.5ml pen
-                Stability 28 days after initial injection
-                Needs pen needles supply
-                Device supplied by manufacturer
-                CIII in CO, ID, MN, NV, RI, WV
-                Sub-Q only
-                """)
-                break
-            else:
-                print("Not in dose range for this medication")
-                break
-    
-elif command == "B":
-    print ("Please provide following information for calculation")
-    Q = int(input("Number of Pens: "))
-    D = float(input("Dose (mg): "))
-    D1 = int(D)
-    I = int(input("Injections per week: "))
-    while True:
-        try:
-            input=int(D)
-        except ValueError:
-            print("That's not a number")
-        else:
-            if 0.05 <= input <= 4:
-                DS = (9.95*Q*(1/D)*(7/I))
-                print ('Days Supply: ',DS)
-                print("""
-                Enter 2 for each 1.5ml pen
-                Stability 28 days after initial injection
-                Needs pen needles supply
-                Device supplied by manufacturer
-                CIII in CO, ID, MN, NV, RI, WV
-                Sub-Q only
-                """)
-                break
-            else:
-                print("Not in dose range for this medication")
-                break
-        
+command = input("Select Drug: ")
+
+if command in hormone_dict:
+    (Q, D, I) = get_inputs()
+    const = hormone_dict[command]
+
+    # Perform Calculation
+    DS = (const * Q * (1 / D) * (7 / I))
+
+    print ('Days Supply:', DS)
+
+    print ("""
+    Enter 2 for each 1.5ml pen
+    Stability 28 days after initial injection
+    Needs pen needles supply
+    Device supplied by manufacturer
+    CIII in CO, ID, MN, NV, RI, WV
+    Sub-Q only
+    """)
 else:
-    print("Error: invalid selection")
-    print(description)   
+    print("Error: invalid selection!")
 
+promptExit = input("Press Enter to Exit...")
+exit(0)
